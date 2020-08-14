11ad4932fc1fbf2848189a289be205ad35a1e612
kzlsakal <dorukgurleyen@gmail.com>
2020-08-14 15:57:50 -0700

    Make port a constant variable

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const PORT = '3000';
+
 const SLN_HOST = 'localhost';
 const SLN_PORT = '3005';
 
@@ -12,4 +14,4 @@ app.get('*/sln-assets/:asset', (req, res) => {
   res.redirect(`//${SLN_HOST}:${SLN_PORT}/sln-assets/${req.params.asset}`);
 });
 
-app.listen(3000, () => console.log(`ReBurke listening at 3000`));
+app.listen(PORT, () => console.log(`ReBurke listening at ${PORT}`));
