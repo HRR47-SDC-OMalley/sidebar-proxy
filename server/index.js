8aae23b3010e889d4ad2ba717b77463bc0d45c84
Hewah Bahrami <hewbahrami@gmail.com>
2020-09-04 20:19:21 -0400

    proxy set up to only serve sidebar service

--- server/index.js
+++ server/index.js
@@ -14,35 +14,35 @@ const CLOUD_STYLE_URL = process.env.CLOUD_STYLE_URL;
  * Sources
  */
 // Main Photo
-const photoTarget = process.env.PHOTO_TARGET
-  || 'http://localhost:3001';
-const photoScriptUrl = process.env.PHOTO_SCRIPT
-  || 'http://localhost:3001/bundle.js';
+// const photoTarget = process.env.PHOTO_TARGET
+//   || 'http://localhost:3001';
+// const photoScriptUrl = process.env.PHOTO_SCRIPT
+//   || 'http://localhost:3001/bundle.js';
 // Sidebar
 const sbTarget = process.env.SB_TARGET
   || 'http://localhost:3210';
 const sbScriptUrl = process.env.SB_SCRIPT
   || 'http://localhost:3210/bundle.js';
 // User Reviews
-const reviewsTarget = process.env.REVIEWS_TARGET
-  || 'http://localhost:2625';
-const reviewsScriptUrl = process.env.REVIEWS_SCRIPT
-  || 'http://localhost:2625/dist/bundle.js';
-// Similar Listing and News
-const slnTarget = process.env.SLN_TARGET
-  || 'http://localhost:3005';
-const slnScriptUrl = process.env.SLN_SCRIPT
-  || 'http://localhost:3005/similar-listings-news.bundle.js';
+// const reviewsTarget = process.env.REVIEWS_TARGET
+//   || 'http://localhost:2625';
+// const reviewsScriptUrl = process.env.REVIEWS_SCRIPT
+//   || 'http://localhost:2625/dist/bundle.js';
+// // Similar Listing and News
+// const slnTarget = process.env.SLN_TARGET
+//   || 'http://localhost:3005';
+// const slnScriptUrl = process.env.SLN_SCRIPT
+//   || 'http://localhost:3005/similar-listings-news.bundle.js';
 
 /**
  * Serve template
  */
 app.get('/item/:id', (req, res) => {
   res.end(pug.renderFile(path.resolve(__dirname, './../client/listing.pug'), {
-    photoScriptUrl,
+    // photoScriptUrl,
     sbScriptUrl,
-    reviewsScriptUrl,
-    slnScriptUrl
+    // reviewsScriptUrl,
+    // slnScriptUrl
   }));
 });
 
@@ -62,8 +62,8 @@ if (CLOUD_STYLE_URL) {
 /**
  * Main Photo Proxy
  */
-const photoProxy = { target: photoTarget, changeOrigin: true };
-app.use('*/photo/api/*', createProxyMiddleware(photoProxy));
+// const photoProxy = { target: photoTarget, changeOrigin: true };
+// app.use('*/photo/api/*', createProxyMiddleware(photoProxy));
 
 /**
  * Sidebar Proxy
@@ -74,13 +74,13 @@ app.use('*/sb/api/*', createProxyMiddleware(sbProxy));
 /**
  * Seller Reviews Proxy
  */
-const reviewsProxy = { target: reviewsTarget, changeOrigin: true };
-app.use('*/reviews/api/*', createProxyMiddleware(reviewsProxy));
+// const reviewsProxy = { target: reviewsTarget, changeOrigin: true };
+// app.use('*/reviews/api/*', createProxyMiddleware(reviewsProxy));
 
 /**
  * Similar Listings & Related News Proxy
  */
-const slnProxy = { target: slnTarget, changeOrigin: true };
-app.use('*/sln/api/*', createProxyMiddleware(slnProxy));
+// const slnProxy = { target: slnTarget, changeOrigin: true };
+// app.use('*/sln/api/*', createProxyMiddleware(slnProxy));
 
 app.listen(PORT, () => console.log(`ReBurke listening on ${URL}:${PORT}`));
